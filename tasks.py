834b3f1e859fb9d361a93a14024b019805fb252e
Raja <rajababuchauhan500@gmail.com>
2026-08-31 15:33:58 +0000

    ai-chats dom ops logger bug fixes

--- tasks.py
+++ tasks.py
@@ -193,5 +193,4 @@ def deploy_backend(c, repo=BACKEND_REPO, up=True, no_cache=False, remove_orphans
 
 # inv deploy -r git@github.com:chauhan112/doa-frontend-react.git -n "doa" --no-register
 
-
 # inv deploy -r git@github.com:chauhan112/scanner-frontend.git -n "scanner" --no-register
